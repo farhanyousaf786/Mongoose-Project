cc5a1e3f9bfb23102f925cde36d49b8502925d8f
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-18 15:30:27 -0400

    Deploy to heroku

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -6,16 +6,13 @@ module.exports = {
 
 function create(req, res) {
     Mobile.findById(req.params.id, function (err, feedbackDocument) {
-        console.log("movie docssssssssss >>>>>>>>", feedbackDocument);
         req.body.user = req.user._id;
         req.body.userName = req.user.name;
         req.body.userAvatar = req.user.avatar;
         feedbackDocument.feedback.push(req.body);
-
         feedbackDocument.save(function (err) {
             res.redirect(`/mobiles/${req.params.id}`);
         });
-        console.log("movie docssssssssss >>>>>>>>", feedbackDocument);
 
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { resource } = require('../server');
 
 
 // The root route renders our only view
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   //UPDATE THIS
   // Where do you want to go for the root route
   // in the student demo this was res.redirect('/movies'), what do you want?
@@ -27,14 +27,14 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/mobiles', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/mobiles' //  UPDATE THIS, where do you want the client to go if login fails
+    successRedirect: '/mobiles', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect: '/mobiles' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
-router.get('/logout', function(req, res){
-  req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
+router.get('/logout', function (req, res) {
+  req.logout(function () { //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
     res.redirect('/')
   })
 })
